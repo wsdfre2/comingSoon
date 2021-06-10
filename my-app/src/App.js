0ea0c5f717c461f513d2802afb3da48e5fec2e6b
Young Cha <youngc0905@gmail.com>
2021-06-09 17:11:59 -0700

    added reactapp

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src='Gulp_Logo_3.1.jpg' />
+        <p>Coming soon ...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
